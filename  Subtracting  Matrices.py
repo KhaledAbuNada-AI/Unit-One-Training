58b651c79ff06556dbf17ae16e51a047dbd37712
KhaledAudNada <44647104+120180163@users.noreply.github.com>
2021-08-11 12:02:53 +0300

    Add files via upload

---
+++  Subtracting  Matrices.py
@@ -0,0 +1,18 @@
+# importing numpy as np
+import numpy as np
+
+
+# creating first matrix
+matrix1 = np.array([[3, 3], [5, 4]])
+
+# creating second matrix
+matrix2 = np.array([[9, 5], [2, 4]])
+
+print("Printing elements of first matrix")
+print(matrix1)
+print("Printing elements of second matrix")
+print(matrix2)
+
+# adding two matrix
+print("Addition of two matrix")
+print(np.subtract(matrix1, matrix2))
